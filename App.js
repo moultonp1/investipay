fa57a1b95e5da6f242618f23fdd8083d2e3bd1c0
moultonp1 <160091004+moultonp1@users.noreply.github.com>
2025-02-14 13:22:52 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FPSGame from './FPSGame';  // Make sure the filename matches your game file
+
+function App() {
+  return (
+    <div>
+      <FPSGame />
+    </div>
+  );
+}
+
+export default App;
